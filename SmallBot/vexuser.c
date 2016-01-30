1fadc06f428f49186c30715dfce39eaeb760b21a
Azeem Arshad <azeem.arshadt@gmail.com>
2016-01-30 13:16:57 -0800

    indentation

--- SmallBot/vexuser.c
+++ SmallBot/vexuser.c
@@ -68,25 +68,25 @@
 
 // Digi IO configuration
 static  vexDigiCfg  dConfig[] = {
-        { P_ENC_BOT_FLY_A,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc1,    kVexQuadEncoder_1},
-        { P_ENC_BOT_FLY_B,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc2,    kVexQuadEncoder_1},
+  { P_ENC_BOT_FLY_A,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc1,    kVexQuadEncoder_1},
+  { P_ENC_BOT_FLY_B,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc2,    kVexQuadEncoder_1},
 
-        { P_ENC_TOP_FLY_A,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc1,    kVexQuadEncoder_2},
-        { P_ENC_TOP_FLY_B,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc2,    kVexQuadEncoder_2}
+  { P_ENC_TOP_FLY_A,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc1,    kVexQuadEncoder_2},
+  { P_ENC_TOP_FLY_B,      kVexSensorQuadEncoder ,  kVexConfigQuadEnc2,    kVexQuadEncoder_2}
 };
 
 // Motor Config
 static  vexMotorCfg mConfig[] = {
-    { M_DRIVE_LEFT1,    kVexMotor393S,           kVexMotorReversed,     kVexSensorIME,         kImeChannel_2 },
-    { M_DRIVE_RIGHT1,   kVexMotor393S,           kVexMotorNormal,     kVexSensorIME,         kImeChannel_1 },
-    { M_FEED_SHOOT,     kVexMotor393S,           kVexMotorNormal,     kVexSensorNone,        0 },
+  { M_DRIVE_LEFT1,    kVexMotor393S, kVexMotorReversed, kVexSensorIME,  kImeChannel_2 },
+  { M_DRIVE_RIGHT1,   kVexMotor393S, kVexMotorNormal,   kVexSensorIME,  kImeChannel_1 },
+  { M_FEED_SHOOT,     kVexMotor393S, kVexMotorNormal,   kVexSensorNone, 0 },
 
-    { M_FLY_TOP_WHEEL,      kVexMotor393T,      kVexMotorNormal,   kVexSensorQuadEncoder, kVexQuadEncoder_1 },
-    { M_FLY_BOT_WHEEL,     kVexMotor393T,      kVexMotorNormal,     kVexSensorQuadEncoder, kVexQuadEncoder_2 },
+  { M_FLY_TOP_WHEEL,  kVexMotor393T, kVexMotorNormal, kVexSensorQuadEncoder, kVexQuadEncoder_2 },
+  { M_FLY_BOT_WHEEL,  kVexMotor393T, kVexMotorNormal, kVexSensorQuadEncoder, kVexQuadEncoder_1 },
 
-    { M_FEED_FRONT,     kVexMotor393S,           kVexMotorReversed,     kVexSensorNone,        0 },
-    { M_DRIVE_LEFT2,    kVexMotor393S,           kVexMotorNormal,     kVexSensorNone,        0 },
-    { M_DRIVE_RIGHT2,   kVexMotor393S,           kVexMotorNormal,     kVexSensorNone,        0 }
+  { M_FEED_FRONT,     kVexMotor393S, kVexMotorReversed, kVexSensorNone, 0 },
+  { M_DRIVE_LEFT2,    kVexMotor393S, kVexMotorNormal,   kVexSensorNone, 0 },
+  { M_DRIVE_RIGHT2,   kVexMotor393S, kVexMotorNormal,   kVexSensorNone, 0 }
 };
 
 
@@ -95,88 +95,88 @@ TBHController *topWheelCtrl;
 TBHController *botWheelCtrl;
 
 bool driveMotors(void) {
-    short ld, rd ;
-    //Calculate Motor Power
-    int forward = VALLEY(vexControllerGet(J_DRIVE), 45, 127);
-    int turn    = VALLEY(vexControllerGet(J_TURN), 45, 127);
-    ld = VALLEY(forward + turn, 45, 127);
-    rd = VALLEY(forward - turn, 45, 127);
+  short ld, rd ;
+  //Calculate Motor Power
+  int forward = VALLEY(vexControllerGet(J_DRIVE), 45, 127);
+  int turn    = VALLEY(vexControllerGet(J_TURN), 45, 127);
+  ld = VALLEY(forward + turn, 45, 127);
+  rd = VALLEY(forward - turn, 45, 127);
 
-    vexMotorSet(M_DRIVE_LEFT1,  ld);
-    vexMotorSet(M_DRIVE_LEFT2,  ld);
-    vexMotorSet(M_DRIVE_RIGHT1, rd);
-    vexMotorSet(M_DRIVE_RIGHT2, rd);
-	
-    return (ld != 0 || rd != 0);
+  vexMotorSet(M_DRIVE_LEFT1,  ld);
+  vexMotorSet(M_DRIVE_LEFT2,  ld);
+  vexMotorSet(M_DRIVE_RIGHT1, rd);
+  vexMotorSet(M_DRIVE_RIGHT2, rd);
+
+  return (ld != 0 || rd != 0);
 }
 
 bool isBallOuter(void) {
-	return (vexAdcGet(S_BALL_OUT) < 500);
+  return (vexAdcGet(S_BALL_OUT) < 500);
 }
 
 bool isBallInner(void) {
-	return (vexAdcGet(S_BALL_IN) < 500);
+  return (vexAdcGet(S_BALL_IN) < 500);
 }
 
 bool isBallInner2(void) {
-	return (vexAdcGet(S_BALL_IN2) < 500);
+  return (vexAdcGet(S_BALL_IN2) < 500);
 }
 
 void
 vexUserSetup()
 {
-	vexDigitalConfigure( dConfig, DIG_CONFIG_SIZE( dConfig ) );
-	vexMotorConfigure( mConfig, MOT_CONFIG_SIZE( mConfig ) );
+  vexDigitalConfigure( dConfig, DIG_CONFIG_SIZE( dConfig ) );
+  vexMotorConfigure( mConfig, MOT_CONFIG_SIZE( mConfig ) );
 }
 
 void
 vexUserInit()
 {
-    topWheelCtrl = TBHControllerInit(S_ENC_TOP_FLY, 0.05, 10500, false);
-    topWheelCtrl->powerZeroClamp = true;
-    topWheelCtrl->log = false;
+  topWheelCtrl = TBHControllerInit(S_ENC_TOP_FLY, 0.05, 10500, false);
+  topWheelCtrl->powerZeroClamp = true;
+  topWheelCtrl->log = false;
 
-    botWheelCtrl = TBHControllerInit(S_ENC_BOT_FLY, 0.05, 10500, false);
-    botWheelCtrl->log = true;
-    botWheelCtrl->powerZeroClamp = true;
+  botWheelCtrl = TBHControllerInit(S_ENC_BOT_FLY, 0.05, 10500, false);
+  botWheelCtrl->log = true;
+  botWheelCtrl->powerZeroClamp = true;
 }
 
 msg_t
 vexAutonomous( void *arg )
 {
-   (void)arg;
-	vexTaskRegister("auton");
-	vex_printf("starting autonomous\n");
-    int shootCount = 0;
-    int step = 0;
-    systime_t lastTime = chTimeNow();
-    while(!chThdShouldTerminate())
-    {
-    	systime_t time = chTimeNow();
-    	int32_t timeGap = time-lastTime;
-    	if(step == 0 && timeGap > 1000) {
-			vex_printf("step 0\n");
-			vexMotorSet(M_DRIVE_LEFT1,  80);
-			vexMotorSet(M_DRIVE_LEFT2,  80);
-			vexMotorSet(M_DRIVE_RIGHT1, 80);
-			vexMotorSet(M_DRIVE_RIGHT2, 80);
-			step++;
-			lastTime = time;
-    	}
-    	else if(step == 1 && timeGap > 2000) {
-			vex_printf("step 1\n");
-			vexMotorSet(M_DRIVE_LEFT1,  0);
-			vexMotorSet(M_DRIVE_LEFT2,  0);
-			vexMotorSet(M_DRIVE_RIGHT1, 0);
-			vexMotorSet(M_DRIVE_RIGHT2, 0);
-    		step++;
-    	} else if(step == 2) {
-    		break;
-    	}
-        vexSleep( 10 );
+  (void)arg;
+  vexTaskRegister("auton");
+  vex_printf("starting autonomous\n");
+  int shootCount = 0;
+  int step = 0;
+  systime_t lastTime = chTimeNow();
+  while(!chThdShouldTerminate())
+  {
+    systime_t time = chTimeNow();
+    int32_t timeGap = time-lastTime;
+    if(step == 0 && timeGap > 1000) {
+      vex_printf("step 0\n");
+      vexMotorSet(M_DRIVE_LEFT1,  80);
+      vexMotorSet(M_DRIVE_LEFT2,  80);
+      vexMotorSet(M_DRIVE_RIGHT1, 80);
+      vexMotorSet(M_DRIVE_RIGHT2, 80);
+      step++;
+      lastTime = time;
     }
-	vex_printf("End\n");
-    return (msg_t)0;
+    else if(step == 1 && timeGap > 2000) {
+      vex_printf("step 1\n");
+      vexMotorSet(M_DRIVE_LEFT1,  0);
+      vexMotorSet(M_DRIVE_LEFT2,  0);
+      vexMotorSet(M_DRIVE_RIGHT1, 0);
+      vexMotorSet(M_DRIVE_RIGHT2, 0);
+      step++;
+    } else if(step == 2) {
+      break;
+    }
+    vexSleep( 10 );
+  }
+  vex_printf("End\n");
+  return (msg_t)0;
 }
 
 
@@ -184,89 +184,89 @@ vexAutonomous( void *arg )
 /** @brief      Driver control                                                 */
 /*-----------------------------------------------------------------------------*/
 /** @details
- *  This thread is started when the driver control period is started
- */
+*  This thread is started when the driver control period is started
+*/
 msg_t
 vexOperator( void *arg )
 {
-	(void)arg;
-	// Must call this
-	vexTaskRegister("operator");
+  (void)arg;
+  // Must call this
+  vexTaskRegister("operator");
 
 
-	// Run until asked to terminate
-    int  motorRunningTime = 0;
-    bool ballFeed = false;
-	while(!chThdShouldTerminate())
-	{
-		if(isBallInner2()) {
-			ballFeed = true;
-		}
-		if(isBallOuter()) {
-			ballFeed = false;
-		}
+  // Run until asked to terminate
+  int  motorRunningTime = 0;
+  bool ballFeed = false;
+  while(!chThdShouldTerminate())
+  {
+    if(isBallInner2()) {
+      ballFeed = true;
+    }
+    if(isBallOuter()) {
+      ballFeed = false;
+    }
 
-        bool motorRunning = driveMotors();
-        if(motorRunning) {
-            motorRunningTime = 25;
-        } else {
-            motorRunningTime = MAX(motorRunningTime-1, 0);
-        }
-        if(motorRunningTime > 0) {
-            motorRunning = true;
-        }
+    bool motorRunning = driveMotors();
+    if(motorRunning) {
+      motorRunningTime = 25;
+    } else {
+      motorRunningTime = MAX(motorRunningTime-1, 0);
+    }
+    if(motorRunningTime > 0) {
+      motorRunning = true;
+    }
 
-        if(vexControllerGet(J_SHOOT_PB)) {
-        	tbhEnable(topWheelCtrl, FLY_PB_SPEED);
-        	tbhEnable(botWheelCtrl, FLY_PB_SPEED);
-        }
-        if(vexControllerGet(J_SHOOT_MAX)) {
-        	tbhEnable(topWheelCtrl, FLY_MAX_SPEED);
-        	tbhEnable(botWheelCtrl, FLY_MAX_SPEED);
-        }
-        if(vexControllerGet(J_SHOOT_75)) {
-        	tbhEnable(topWheelCtrl, FLY_75_SPEED);
-        	tbhEnable(botWheelCtrl, FLY_75_SPEED);
-        }
-        if(vexControllerGet(J_SHOOT_SIDE)) {
-        	tbhEnable(topWheelCtrl, FLY_SIDE_SPEED);
-        	tbhEnable(botWheelCtrl, FLY_SIDE_SPEED);
-        }
-        if(vexControllerGet(J_SHOOT_STOP)) {
-        	tbhDisable(topWheelCtrl);
-        	tbhDisable(botWheelCtrl);
-        }
-        vexMotorSet(M_FLY_TOP_WHEEL, tbhUpdate(topWheelCtrl));
-        vexMotorSet(M_FLY_BOT_WHEEL, tbhUpdate(botWheelCtrl));
+    if(vexControllerGet(J_SHOOT_PB)) {
+      tbhEnable(topWheelCtrl, FLY_PB_SPEED);
+      tbhEnable(botWheelCtrl, FLY_PB_SPEED);
+    }
+    if(vexControllerGet(J_SHOOT_MAX)) {
+      tbhEnable(topWheelCtrl, FLY_MAX_SPEED);
+      tbhEnable(botWheelCtrl, FLY_MAX_SPEED);
+    }
+    if(vexControllerGet(J_SHOOT_75)) {
+      tbhEnable(topWheelCtrl, FLY_75_SPEED);
+      tbhEnable(botWheelCtrl, FLY_75_SPEED);
+    }
+    if(vexControllerGet(J_SHOOT_SIDE)) {
+      tbhEnable(topWheelCtrl, FLY_SIDE_SPEED);
+      tbhEnable(botWheelCtrl, FLY_SIDE_SPEED);
+    }
+    if(vexControllerGet(J_SHOOT_STOP)) {
+      tbhDisable(topWheelCtrl);
+      tbhDisable(botWheelCtrl);
+    }
+    vexMotorSet(M_FLY_TOP_WHEEL, tbhUpdate(topWheelCtrl));
+    vexMotorSet(M_FLY_BOT_WHEEL, tbhUpdate(botWheelCtrl));
 
-        if(vexControllerGet(J_FEED_FRONT_U)) {
-            vexMotorSet(M_FEED_FRONT, 100);
-        } else if(vexControllerGet(J_FEED_FRONT_D)) {
-            vexMotorSet(M_FEED_FRONT, -100);
-        }  else if(motorRunning && !(isBallInner() && isBallOuter())) {
-			vexMotorSet(M_FEED_FRONT, -DEFAULT_FEED_SPEED);
-		} else {
-            vexMotorSet(M_FEED_FRONT, 0);
-        }
+    if(vexControllerGet(J_FEED_FRONT_U)) {
+      vexMotorSet(M_FEED_FRONT, 100);
+    } else if(vexControllerGet(J_FEED_FRONT_D)) {
+      vexMotorSet(M_FEED_FRONT, -100);
+    }  else if(motorRunning && !(isBallInner() && isBallOuter())) {
+      vexMotorSet(M_FEED_FRONT, -DEFAULT_FEED_SPEED);
+    } else {
+      vexMotorSet(M_FEED_FRONT, 0);
+    }
 
-        if(vexControllerGet(J_FEED_SHOOT) || ballFeed) { // || (isBallInner() && !isBallOuter())) { //8D
-            vexMotorSet(M_FEED_SHOOT, -80);
-            vexMotorSet(M_FEED_FRONT, -80);
-        }  else if(motorRunning && !(isBallInner() && isBallOuter())) {
-            vexMotorSet(M_FEED_FRONT, -DEFAULT_FEED_SPEED);
-		} else {
-            vexMotorSet(M_FEED_SHOOT, 0);
-            if(!(vexControllerGet(J_FEED_FRONT_D) || vexControllerGet(J_FEED_FRONT_U))) {
-                vexMotorSet(M_FEED_FRONT, 0);
-            }
-        }
+    if(vexControllerGet(J_FEED_SHOOT) || ballFeed) { // || (isBallInner() && !isBallOuter())) { //8D
+      vexMotorSet(M_FEED_SHOOT, -80);
+      vexMotorSet(M_FEED_FRONT, -80);
+    }  else if(motorRunning && !(isBallInner() && isBallOuter())) {
+      vexMotorSet(M_FEED_FRONT, -DEFAULT_FEED_SPEED);
+    } else {
+      vexMotorSet(M_FEED_SHOOT, 0);
+      if(!(vexControllerGet(J_FEED_FRONT_D) || vexControllerGet(J_FEED_FRONT_U))) {
+        vexMotorSet(M_FEED_FRONT, 0);
+      }
+    }
 
-		if(vexControllerGet(J_FEED_REV)) {
-            vexMotorSet(M_FEED_SHOOT, 127);
-            vexMotorSet(M_FEED_FRONT, 127);
-        }
-        vexSleep( 10 );
-	}
+    if(vexControllerGet(J_FEED_REV)) {
+      vexMotorSet(M_FEED_SHOOT, 127);
+      vexMotorSet(M_FEED_FRONT, 127);
+    }
+    vexSleep( 10 );
+  }
 
-	return (msg_t)0;
+  return (msg_t)0;
 }
