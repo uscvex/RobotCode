359912ce087223a1d9cb60250dbbaf76afe183da
KNGeeting <geeting@usc.edu>
2016-02-14 19:55:14 -0800

    Value changes to slow turn

--- SmallBot/vexuser.c
+++ SmallBot/vexuser.c
@@ -123,15 +123,20 @@ bool driveMotors(void) {
   short ld, rd ;
   //Calculate Motor Power
   int forward = VALLEY(vexControllerGet(J_DRIVE), 10, 127);
-  int turn    = VALLEY(vexControllerGet(J_TURN), 10, 127);
+  int turn;
+  if(vexControllerGet(J_HALF_SPEED))
+  {
+	  turn  = VALLEY(vexControllerGet(J_TURN)*.4, 10, 127);
+  }
+  else
+  {
+	  turn  = VALLEY(vexControllerGet(J_TURN), 10, 127);
+  }
+
   ld = VALLEY(forward + turn, 45, 127);
   rd = VALLEY(forward - turn, 45, 127);
 
-  if(vexControllerGet(J_HALF_SPEED))
-  {
-	  ld = ld/3;
-	  rd = rd/3;
-  }
+
   vexMotorSet(M_DRIVE_LEFT1,  ld);
   vexMotorSet(M_DRIVE_LEFT2,  ld);
   vexMotorSet(M_DRIVE_RIGHT1, rd);
