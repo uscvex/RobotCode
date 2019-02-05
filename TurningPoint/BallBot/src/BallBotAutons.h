5b89166c12147163960660284e893934f72208f7
Sam <samjamesburton@gmail.com>
2019-02-05 15:44:24 -0800

    22-24 Skills working 57s

--- TurningPoint/BallBot/src/BallBotAutons.h
+++ TurningPoint/BallBot/src/BallBotAutons.h
@@ -357,40 +357,40 @@ double skills[] = {
     
     PAUSE,0.25,
     
-    WRISTSEEK,WRIST_FORWARD_POS,        // LOWER FLIPPER
+    /*WRISTSEEK,WRIST_FORWARD_POS,        // LOWER FLIPPER
     
     TURN,50,2,                          // TURN FOR NEXT CAP
     
     DRIVE,-90,50,DISTANCE,0.9,2,        // DRIVE TO CAP
     WRISTSEEK,WRIST_VERTICAL_POS,       // FLIP CAP
     PAUSE,0.5,
-    DRIVE,90,50,DISTANCE,0.5,2,
+    DRIVE,90,50,DISTANCE,0.5,2,*/
     
     TURN,90,2,                          // AIM AT START TILE
     
-    DRIVE,127,90,WHITE_E,2,             // DRIVE BACK TO TILE
-    DRIVE,-127,90,DISTANCE,0.1,0.5,     // DRIVE TO BREAK
+    DRIVE,127,90,SONAR,0.5,2,           // DRIVE BACK TO TILE
+    //DRIVE,-127,90,DISTANCE,0.1,0.5,     // DRIVE TO BREAK
     
     TURN,0,2,                           // TURN TO FACE FLAGS
     
     START_COAST,                        // START FLYWHEEL SPINUP
     
-    DRIVE,90,0,WHITE_B,0.5,
+    DRIVE,90,0,WHITE_L,0.5,
     
-    DRIVE,127,0,0.5,                   // DRIVE A LITTLE
+    DRIVE,127,0,0.5,                    // DRIVE A LITTLE
     
-    DRIVE,127,0,BLACK_B,2,
-    
-    DRIVE,127,0,WHITE_E,2,
+    DRIVE,127,0,BLACK_L,2,
+    DRIVE,127,0,0.2,                    // DRIVE A LITTLE
+    DRIVE,90,0,WHITE_L,2,
                                                                         // 1st Flag Set
     TURN_AIM,BLUE_FLAG,CENTER,2,        // AIM AT LEFT-MOST BLUE FLAG
-    DRIVE,90,CDIR,DISTANCE,0.9,2,       // DRIVE TO TOP FLAG DIST
+    DRIVE,90,CDIR,DISTANCE,0.8,2,         // DRIVE TO TOP FLAG DIST
     FIRE_AIM,TOP,                       // SHOOT TOP FLAG
     PAUSE,FIRED,2,                      // WAIT TILL SHOT
     PAUSE,0.5,
     STOP_FIRE,                          // STOP FLYWHEEL
     
-    DRIVE,127,CDIR,DISTANCE,0.7,2,      // DRIVE TO MIDDLE FLAG DIST
+    DRIVE,127,CDIR,DISTANCE,0.8,2,      // DRIVE TO MIDDLE FLAG DIST
     FIRE_AIM,MIDDLE,                    // SHOOT MIDDLE FLAG
     PAUSE,FIRED,2,                      // WAIT TILL SHOT
     PAUSE,0.5,                          // SHORT PAUSE
@@ -398,24 +398,25 @@ double skills[] = {
     
     STOP_COAST,                         // TURN FLYWHEEL FULLY OFF
     
-    INTAKE_OFF,                         // TURN INTAKE OFF
+    //INTAKE_OFF,                         // TURN INTAKE OFF
     
-    DRIVE,127,0,0.5,                    // DRIVE TO TOGGLE BOTTOM FLAG
+    DRIVE,127,0,0.3,                    // DRIVE TO TOGGLE BOTTOM FLAG
     PAUSE,0.25,                         // SHORT PAUSE
     
     TURN,0,1,                           // TURN STRAIGHT
     
     INTAKE_ON,                          // TURN INTAKE ON
     
-    DRIVE,-127,0,DISTANCE,1,2,          // DRIVE AWAY A LITTLE
-    
-    DRIVE,-127,0,WHITE_L,4,             // DRIVE TO START TILE
+    DRIVE,-127,0,0.3,                   // DRIVE AWAY A LITTLE
+    TURN,0,1,                           // TURN STRAIGHT
+    DRIVE,-127,0,WHITE_L,2,             // DRIVE TO START TILE
     DRIVE,-127,0,DISTANCE,0.5,1,        // DRIVE TO LINE UP
     
     TURN,270,2,                         // TURN FOR NEXT MOVE
     TURN,265,2,                         // TURN FOR NEXT MOVE
     
-    DRIVE,127,270,DISTANCE,1.8,2,       // DRIVE TO GET BALL
+    DRIVE,127,270,BLACK_B,1,
+    DRIVE,127,270,DISTANCE,1.3,2,       // DRIVE TO GET BALL
     INTAKE_ON,
     PAUSE,0.5,
     
@@ -441,35 +442,38 @@ double skills[] = {
     TURN,0,1,                           // TURN TO AIM
     TURN_AIM,BLUE_FLAG,CENTER,2,        // AIM AT CENTRE-MOST BLUE FLAG
                                                                         // 2nd Flag Set
-    IF, GOTBALLS,
-    DRIVE,-90,CDIR,DISTANCE,0.1,1,
-        FIRE_AIM,TOP,                    // SHOOT MIDDLE FLAG
+    //IF, GOTBALLS,
+    DRIVE,-90,CDIR,SONAR,1.2,1,
+        FIRE_AIM,TOP,                    // SHOOT TOP FLAG
         PAUSE,FIRED,2,                      // WAIT TILL SHOT
         PAUSE,0.5,                          // SHORT PAUSE
         STOP_FIRE,                          // STOP FLYWHEEL
-        DRIVE,90,CDIR,DISTANCE,0.2,1,
-    ENDIF,
+    //ENDIF,
     
     TURN_AIM,BLUE_FLAG,CENTER,1,        // AIM AT CENTRE-MOST BLUE FLAG
     
-    DRIVE,127,CDIR,SONAR,1,2,      // DRIVE TO MIDDLE FLAG DIST
-    FIRE_AIM,MIDDLE,                    // SHOOT MIDDLE FLAG
-    PAUSE,FIRED,2,                      // WAIT TILL SHOT
-    PAUSE,0.5,                          // SHORT PAUSE
+    DRIVE,127,CDIR,SONAR,1,2,           // DRIVE TO MIDDLE FLAG DIST
+    IF, GOTBALL,
+        FIRE_AIM,MIDDLE,                    // SHOOT MIDDLE FLAG
+        PAUSE,0.5,                          // SHORT PAUSE
+        PAUSE,FIRED,2,                  // WAIT TILL SHOT
+    ENDIF,
     STOP_FIRE,                          // STOP FLYWHEEL
     
     STOP_COAST,                         // TURN FLYWHEEL FULLY OFF
     
     INTAKE_OFF,                         // TURN INTAKE OFF
     
-    DRIVE,127,0,1,                       // DRIVE TO TOGGLE BOTTOM FLAG
-    PAUSE,0.25,                         // SHORT PAUSE
+    DRIVE,127,0,0.5,                       // DRIVE TO TOGGLE BOTTOM FLAG
+    PAUSE,0.25,                             // SHORT PAUSE
     
     TURN,0,1,                           // TURN STRAIGHT
     
     INTAKE_ON,                          // TURN INTAKE ON
+    DRIVE,-127,0,0.25,
+    TURN,0,1,                           // TURN STRAIGHT
     
-    DRIVE,-127,0,DISTANCE,1.15,2,       // DRIVE AWAY A FROM FLAG
+    DRIVE,-127,0,DISTANCE,0.5,2,        // DRIVE AWAY A FROM FLAG
     
     WRISTSEEK,WRIST_FORWARD_POS,
     TURN,83,2,                          // TURN TO FACE CAP
@@ -482,51 +486,62 @@ double skills[] = {
     DRIVE,127,90,DISTANCE,0.5,1,        // DRIVE AWAY FROM CAP
     
     TURN,180,2,                         // TURN READY FOR NEXT MOVE
-    DRIVE,127,180,DISTANCE,1,2,         // DRIVE TO LINE UP FOR CAP
+    DRIVE,127,180,DISTANCE,0.9,2,       // DRIVE TO LINE UP FOR CAP
     
     TURN,90,2,                          // AIM FOR CAP
-    INTAKE_FLIP,                        // INTAKE REVERSE TO FLIP CAP
+    //INTAKE_FLIP,                        // INTAKE REVERSE TO FLIP CAP
     DRIVE,127,90,DISTANCE,1,2,          // DRIVE TO FLIP CAP
     
     INTAKE_ON,                          // INTAKE ON TO GET BALL
+    
+    START_COAST,
     
     PAUSE,0.5,
     
     DRIVE,-127,90,DISTANCE,1.25,2,      // DRIVE TO LINE UP FOR SHOT
     
-    TURN,0,2,                           // TURN TO FACE FLAGS
-    TURN_AIM,BLUE_FLAG,RIGHT,2,         // AIM AT CENTRE-MOST BLUE FLAG
-    DRIVE,90,CDIR,DISTANCE,0.25,1,
-    
+    TURN,350,2,                           // TURN TO FACE FLAGS
+    TURN_AIM,BLUE_FLAG,LEFT,2,         // AIM AT RIGHT-MOST BLUE FLAG
                                                                         // 3RD FLAG SET
     IF, GOTBALLS,
-        DRIVE,-90,CDIR,DISTANCE,0.2,1,
+        DRIVE,-90,CDIR,SONAR,1.5,1,
+        DRIVE,90,CDIR,SONAR,1.5,1,
         FIRE_AIM,TOP,                    // SHOOT TOP FLAG
         PAUSE,FIRED,2,                      // WAIT TILL SHOT
         PAUSE,0.5,                          // SHORT PAUSE
         STOP_FIRE,                          // STOP FLYWHEEL
-        DRIVE,90,CDIR,DISTANCE,0.2,1,
     ENDIF,
     
-    TURN_AIM,BLUE_FLAG,CENTER,1,        // AIM AT CENTRE-MOST BLUE FLAG
-    
-    DRIVE,127,CDIR,DISTANCE,0.6,2,      // DRIVE TO MIDDLE FLAG DIST
+    TURN_AIM,BLUE_FLAG,CENTER,1,            // AIM AT CENTRE-MOST BLUE FLAG
+    DRIVE,127,CDIR,0.25,
+    DRIVE,127,CDIR,SONAR,1,2,      // DRIVE TO MIDDLE FLAG DIST
     FIRE_AIM,MIDDLE,                    // SHOOT MIDDLE FLAG
     PAUSE,FIRED,2,                      // WAIT TILL SHOT
     PAUSE,0.5,                          // SHORT PAUSE
     STOP_FIRE,                          // STOP FLYWHEEL
     
+    WRISTSEEK,WRIST_VERTICAL_POS,
+    
     STOP_COAST,                         // TURN FLYWHEEL FULLY OFF
     
     INTAKE_OFF,                         // TURN INTAKE OFF
     
-    DRIVE,70,0,1,                       // DRIVE TO TOGGLE BOTTOM FLAG
+    DRIVE,127,0,0.5,                    // DRIVE TO TOGGLE BOTTOM FLAG
     PAUSE,0.25,                         // SHORT PAUSE
     
     TURN,0,1,                           // TURN STRAIGHT
     
-    DRIVE,-127,0,DISTANCE,0.5,2,        // DRIVE AWAY A LITTLE
+    DRIVE,-127,0,0.5,                   // DRIVE AWAY A LITTLE
+    DRIVE,-127,0,DISTANCE,1.2,2,
+    TURN,270,2,
+    DRIVE,127,270,WHITE_R,2,
+    TURN,180,2,
+    DRIVE,127,180,BLACK_L,2,
+    DRIVE,-90,180,0.1,
+    TURN,85,2,
     
+    DRIVE,127,90,DISTANCE,0.5,1,       // PARK
+    DRIVE,127,90,DISTANCE,3,4,
     END
 };
 
