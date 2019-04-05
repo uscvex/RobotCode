871765ff41dd3fa408f7e24946a89d289ae6088c
54mb <sjburton@usc.edu>
2019-04-05 16:47:40 -0700

    Turn code updated

--- TurningPoint/BallBot/src/opcontrol.cpp
+++ TurningPoint/BallBot/src/opcontrol.cpp
@@ -955,7 +955,9 @@ void setScraperPos(double pos) {
     scraperOffset = pos;
     scraperPos = pos;
 }
-
+pros::vision_signature_s_t GREEN_SIG;
+pros::vision_signature_s_t RED_SIG;
+pros::vision_signature_s_t BLUE_SIG;
 
 void calibrateVision() {
     
@@ -967,24 +969,24 @@ void calibrateVision() {
     // Calibration for Red Bot
     else {
         
-        pros::vision_signature_s_t BLUE_SIG =
-        pros::Vision::signature_from_utility(BLUE_FLAG, -2469, -611, -1540, 203, 7341, 3772, 0.5, 1);
+        BLUE_SIG =
+        pros::Vision::signature_from_utility(BLUE_FLAG, -2469, -611, -1540, 203, 7341, 3772, 0.9, 1);
         
-        pros::vision_signature_s_t RED_SIG =
+        RED_SIG =
         pros::Vision::signature_from_utility(RED_FLAG, 1283, 3237, 2260, -415, 1, -207, 1.1, 1);
         
-        pros::vision_signature_s_t GREEN_SIG =
+        GREEN_SIG =
         pros::Vision::signature_from_utility(GREEN_FLAG, -2685, -1579, -2132, -3263, -1853, -2558, 1.4, 1);
         
         
-        camera.set_signature(BLUE_FLAG, &BLUE_SIG);
-        camera.set_signature(RED_FLAG, &RED_SIG);
-        camera.set_signature(GREEN_FLAG, &GREEN_SIG);
+//        camera.set_signature(BLUE_FLAG, &BLUE_SIG);
+//        camera.set_signature(RED_FLAG, &RED_SIG);
+//        camera.set_signature(GREEN_FLAG, &GREEN_SIG);
         
         camera.set_exposure(70);
     }
     
-    camera.set_zero_point(pros::E_VISION_ZERO_CENTER);
+    camera.set_zero_point(pros::E_VISION_ZERO_TOPLEFT);
     
 }
 
@@ -1752,6 +1754,7 @@ double getRelativeAngle(int location = CENTER, int target = DEFAULT) {
         lookingFor = target;
     
     std::vector<vision_object_s_t> allThings;
+    std::vector<vision_object_s_t> greenThings;
     
     int noObjs = camera.get_object_count();                     // Find number of objects visable
     
@@ -1763,26 +1766,49 @@ double getRelativeAngle(int location = CENTER, int target = DEFAULT) {
         if (thisThing.signature == lookingFor) {                // The type of object it is
             allThings.push_back(thisThing);                     // And stick it into a vector
         }
+        if (thisThing.signature == GREEN_FLAG) {                // Put any green objects into vector
+            greenThings.push_back(thisThing);
+        }
     }
     
     
     if (allThings.size() == 0) return 0;    // No correct objects found, so don't aim
     
-    // Now check objects to delete any imposters - or don't aim if too close
     
+    // Now check objects to delete any imposters
     for (int i = 0; i < allThings.size(); i++) {
         if (lookingFor == BLUE_FLAG || lookingFor == RED_FLAG) {
-            // Check if too big/close
-            if (allThings[i].width > MAX_FLAG_WIDTH || allThings[i].height > MAX_FLAG_HEIGHT) {
-                allThings.erase(allThings.begin() + i);
+            // Check if green object is within range
+            bool greenWithinRange = false;
+            for (int j = 0; j < greenThings.size(); j++) {
+                // Reference frame is 0,0 top left, increasing right and down D:
+                if (lookingFor == BLUE_FLAG) {
+                    if (
+                        greenThings[j].y_middle_coord > allThings[i].top_coord
+                        &&
+                        greenThings[j].y_middle_coord < allThings[i].top_coord + allThings[i].height
+                        &&
+                        greenThings[j].left_coord + greenThings[j].width < allThings[i].x_middle_coord
+                        &&
+                        greenThings[j].left_coord > allThings[i].left_coord - allThings[i].width
+                        ) {
+                        greenWithinRange = true;
+                        break;
+                    }
+                }
+                else {
+                    
+                }
             }
-            // Check if too low
-            if (allThings[i].y_middle_coord < MIN_FLAG_Y) {
+            
+            if (!greenWithinRange) {// && greenThings.size() > 0) {
                 allThings.erase(allThings.begin() + i);
+                i--;
             }
+            
         }
     }
-    
+
     if (allThings.size() == 0) return 0;    // Check if we've deleted all the things
     
     // Now find the thing furthest right or left, or the closest to the center
@@ -1791,7 +1817,7 @@ double getRelativeAngle(int location = CENTER, int target = DEFAULT) {
     if (location == CENTER) {
         closestDist = 10000;
         for (int i = 0; i < allThings.size(); i++) {
-            if (abs(allThings[i].x_middle_coord) < closestDist) {
+            if (abs(allThings[i].x_middle_coord - (VISION_FOV_WIDTH/2)) < closestDist) {
                 closestDist = allThings[i].x_middle_coord;
             }
         }
@@ -1816,6 +1842,8 @@ double getRelativeAngle(int location = CENTER, int target = DEFAULT) {
     // Aim at the edge of the flag for better chance of toggleing
     if (lookingFor == RED_FLAG) closestDist += FLAG_OFFSET;
     if (lookingFor == BLUE_FLAG) closestDist -= FLAG_OFFSET;
+    
+    closestDist = closestDist - (VISION_FOV_WIDTH/2);
     
     return -closestDist/VISION_SEEK_RATE;
     
